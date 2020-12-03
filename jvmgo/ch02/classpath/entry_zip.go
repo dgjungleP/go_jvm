a0865154db5fe9b1acedc4ef9bd3af4498113c0d
dengguijiang <dgjungle_c@163.com>
2020-12-03 16:15:49 +0800

    ch02 how the classpath work

---
+++ jvmgo/ch02/classpath/entry_zip.go
@@ -0,0 +1,47 @@
+package classpath
+
+import (
+	"archive/zip"
+	"errors"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type ZipEntry struct {
+	absPath string
+}
+
+func newZipEntry(path string) *ZipEntry {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	return &ZipEntry{absPath: absPath}
+}
+
+func (dir *ZipEntry) readClass(className string) ([]byte, Entry, error) {
+	r, err := zip.OpenReader(dir.absPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer r.Close()
+	for _, f := range r.File {
+		if f.Name == className {
+			rc, err := f.Open()
+			if err != nil {
+				return nil, nil, err
+			}
+			defer rc.Close()
+			data, err := ioutil.ReadAll(rc)
+			if err != nil {
+				return nil, nil, err
+			}
+			return data, dir, nil
+		}
+	}
+	return nil, nil, errors.New("class not found: " + className)
+}
+
+func (dir *ZipEntry) String() string {
+	return dir.absPath
+}
